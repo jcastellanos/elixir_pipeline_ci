60ff6c71e867f937a0c3d2ead3e088132ceeeb2d
Juan Castellanos <juancastellanosm@gmail.com>
2023-11-27 17:43:58 -0500

    Github actions pipeline

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule PipelineCi.MixProject do
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
-      coveralls: :test,
+        coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
         "coveralls.html": :test,
